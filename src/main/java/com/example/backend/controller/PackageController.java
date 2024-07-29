edd4037358f8ecf3329f3bc3c2bb4b39deb8255b
JeevaregaiShanmugam <jeevaregaishanmugam.25it@licet.ac.in>
2024-07-29 20:10:07 +0530

    Package commit

---
+++ src/main/java/com/example/backend/controller/PackageController.java
@@ -0,0 +1,31 @@
+package com.example.backend.controller;
+
+import com.example.backend.model.Packages;
+import com.example.backend.service.CartService;
+import com.example.backend.service.PackageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/package")
+@CrossOrigin(origins = "*")
+public class PackageController {
+
+    @Autowired
+    PackageService packageService;
+
+    @Autowired
+    CartService cartService;
+
+    @GetMapping("/getAllPackages")
+    public Iterable<Packages> getAllPackages() {
+        return  packageService.getAllPackages();
+    }
+
+
+    @PostMapping("/buyPackage")
+    public String buyPackage(@RequestBody int packageID) {
+        return cartService.buyPackage(packageID);
+    }
+
+}
